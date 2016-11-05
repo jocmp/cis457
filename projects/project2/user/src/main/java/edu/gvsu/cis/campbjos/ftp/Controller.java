f9f25e305b558496a4d9a83dd9eea8cca4a0c30d
JaxonWright <jaxonhwright@gmail.com>
2016-11-05 15:35:17 -0400

    GUI: Controller class now has skeleton for our current JavaFX GUI

--- projects/project2/user/src/main/java/edu/gvsu/cis/campbjos/ftp/Controller.java
+++ projects/project2/user/src/main/java/edu/gvsu/cis/campbjos/ftp/Controller.java
@@ -1,4 +1,45 @@
 package edu.gvsu.cis.campbjos.ftp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextArea output;
+
+    @FXML
+    private TextArea serverHostname;
+
+    @FXML
+    private TextArea hostname;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextArea port;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private TextField keyword;
+
+    @FXML
+    private ChoiceBox<?> speed;
+
+    @FXML
+    private TextField command;
+
+    @FXML
+    private TextArea username;
+
+    @FXML
+    private TableView<?> resultsTable;
+
 }
